011ff925a887291380e380878251020489abebe9
adriane <adrianedacosta21@gmail.com>
2022-03-25 21:26:18 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int primeiroNumero, segundoNumero, soma;
+Console.WriteLine("soma de dois numeros");
+Console.Write ("digite o primeiro Numero: ");
+primeiroNumero= Convert.ToInt32 (Console.ReadLine());
+ 
+Console.Write ("digite o segundo Numero: ");
+segundoNumero= Convert.ToInt32 (Console.ReadLine());
+
+ soma = primeiroNumero + segundoNumero; 
+
+Console.WriteLine($"\nSoma: {soma}");
